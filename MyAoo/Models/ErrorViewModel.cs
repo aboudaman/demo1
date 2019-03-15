96bc8b298af667966d0087262394aadac16243e3
aboudaman <adaman2000@gmail.com>
2019-03-15 15:52:39 -0400

    Add project files.

---
+++ MyAoo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyAoo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
